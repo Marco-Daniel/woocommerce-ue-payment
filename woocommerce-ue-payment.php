65d92dec3e375c6af2110d04267ca500ec53a3f4
Marco Daniël Leguijt <m.leguijt@outlook.com>
2020-06-19 11:41:51 +0200

    accesClient functionality implemented

--- woocommerce-ue-payment.php
+++ woocommerce-ue-payment.php
@@ -54,14 +54,32 @@ function ue_wc_gateway_init_class() {
             $this->testmode = 'yes' === $this->get_option( 'testmode' );
             $this->use_accessclient = 'yes' === $this->get_option('use_accessclient');
 
-            $this->root_url = $this->testmode ? 'https://demo.cyclos.org/' : "https://mijn.circuitnederland.nl/";
-            $this->username = $this->testmode ? "ticket" : $this->get_option( 'api_username' );
-            $this->password = $this->testmode ? "1234" : $this->get_option( 'api_password' );
+            $this->root_url = $this->testmode ? 'https://demo.cyclos.org' : "https://mijn.circuitnederland.nl";
+            $this->username = $this->testmode ? "ticket" : $this->get_option( 'username' );
+            $this->password = $this->testmode ? "1234" : $this->get_option( 'password' );
             $this->accessclient = $this->use_accessclient ? $this->get_option( 'accessclient' ) : NULL;
  
+            // if accessclient is retrieved.
+            if( !empty($_POST['accessClientCode'])) {
+                $accesscode = $_POST['accessClientCode'];
+                $token = generateAccessclientToken("{$this->root_url}/api", $accesscode, $this->username, $this->password);
+                $this->update_option('accessclient', $token);
+                $this->update_option('use_accessclient', 'yes');
+                $GLOBALS['GeneratedAccessclientCode'] = $token;
+            }
+            
+            // make settings global
+            $GLOBALS['ue_config'] = [
+                'root'          => $this->root_url,
+                'api_root'      => "{$this->root_url}/api",
+                'accessClient'  => $this->accessclient,
+                'user'          => $this->username,
+                'password'      => $this->password,
+            ];
+            $GLOBALS['payment_title'] = $this->title;
 
             // This action hook saves the settings
-	        add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
+            add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
         }
 
         //Function to generate HTML for accessClient generator
@@ -90,12 +108,12 @@ function ue_wc_gateway_init_class() {
 						<legend class="screen-reader-text"><span><?php echo wp_kses_post( $data['title'] ); ?></span></legend>
 						<form method="get" name="accessClientForm" id="accessClientForm" action="">
 							<input type="text" id="accessClientCode" name="accessClientCode" placeholder="AccessClient Activatie Code">
-							<button onclick="$('#accessClientForm').submit();" class="<?php echo esc_attr( $data['class'] ); ?>" type="submit" name="<?php echo esc_attr( $field ); ?>" id="<?php echo esc_attr( $field ); ?>" style="<?php echo esc_attr( $data['css'] ); ?>" <?php echo $this->get_custom_attribute_html( $data ); ?>>Genereer AccessClient</button>
+							<button type="submit" class="<?php echo esc_attr( $data['class'] ); ?>" type="submit" name="<?php echo esc_attr( $field ); ?>" id="<?php echo esc_attr( $field ); ?>" style="<?php echo esc_attr( $data['css'] ); ?>" <?php echo $this->get_custom_attribute_html( $data ); ?>>Genereer AccessClient</button>
 							<p id="accessClientKey"class="description" style="color:red;"><?php echo $GLOBALS['GeneratedAccessClientToken']; ?></p>
 							<p class="description">
                                 <u>Vul uw activatiecode in en klik op Genereer Accesscode </u> 
                                 <br> Log in op uw Utrechtse Euro account en ga naar:
-                                <br> Persoonlijk > Webshop > toegangscodes > Toevoegen > [Vul een beschrijving in] > Opslaan > Activatiecode > Bevestigen
+                                <br> Persoonlijk > Instellingen > Webshop koppelingen > toegangscodes > Toevoegen > [Vul een beschrijving in] > Opslaan > Activatiecode > Bevestigen
                             </p>
 							<?php echo $this->get_description_html( $data ); ?>
 						</form>
@@ -109,6 +127,10 @@ function ue_wc_gateway_init_class() {
         // Plugin options
  		public function init_form_fields(){
             $this->form_fields = array(
+                'basic_settings_title' => array(
+					'title'       => __( 'Basis instellingen' ),
+					'type'        => 'title',
+				),
                 'enabled' => array(
                     'title'         => 'Activeer/Deactiveer',
                     'label'         => 'Activeer UE Gateway',
@@ -149,6 +171,11 @@ function ue_wc_gateway_init_class() {
                     'type'          => 'text',
                     'description'   => 'Vergeet niet na het genereren van uw code om deze op te slaan door onderaan de pagina op Opslaan te klikken!',
                 ),
+                'display_settings_title' => array(
+					'title'       => __( 'Weergave instellingen' ),
+					'type'        => 'title',
+					'description' => 'Pas hier uw weergave instellingen van deze plugin aan. In veel gevallen zijn de standaard waardes voldoende.',
+				),
                 'title' => array(
                     'title'         => 'Titel',
                     'type'          => 'text',
@@ -162,7 +189,41 @@ function ue_wc_gateway_init_class() {
                     'default'       => 'Betaal met Utrechtse Euro\'s.',
                 ),
             );
-	 	}
+         }
+         
+         //Admin options validation and processing.	
+		public function process_admin_options(){
+			if ($_POST['woocommerce_ue_use_accessclient'] == true) {
+				if ($_POST['woocommerce_ue_testmode'] == true) {
+                    WC_Admin_Settings::add_error( 'Error: accessClient not available in testmode.' );
+                    return false;
+				} else {
+					if (empty($_POST['woocommerce_ue_accessclient'])){
+						WC_Admin_Settings::add_error( 'Error: Uw accessClient is niet geldig.' );
+						return false;
+					} else {
+						parent::process_admin_options();
+						return true;
+					}
+				}
+			} else {
+				if ($_POST['woocommerce_ue_testmode'] == true) {
+                    parent::process_admin_options();
+                    return true;
+				} else {
+					if (empty($_POST['woocommerce_ue_username'])){
+						WC_Admin_Settings::add_error( 'Error: Uw gebruikersnaam is niet geldig.' );
+						return false;
+					} else if (empty($_POST['woocommerce_ue_password'])){
+						WC_Admin_Settings::add_error( 'Error: Uw wachtwoord is niet geldig.' );
+						return false;
+					} else {
+						parent::process_admin_options();
+						return true;
+					}
+				}
+			}
+		}
  
         // You will need it if you want your custom credit card form, Step 4 is about it
 		public function payment_fields() {
@@ -184,6 +245,69 @@ function ue_wc_gateway_init_class() {
 		public function webhook() { 
 	 	}
  	}
+}
+
+// helper functions
+function generateAccessclientToken( $base_url, $accesscode, $username, $password ) {
+
+    $url = "{$base_url}/clients/activate?code={$accesscode}";
+
+    $headers = array(
+        'Content-Transfer-Encoding' => 'application/json',
+        'Authorization' => 'Basic '. base64_encode("{$username}:{$password}")
+    );
+    
+    $response = wp_remote_request( $url, array(
+        'method'      => 'POST',
+        'timeout'     => 45,
+        'headers'     => $headers,
+        'sslverify'   => false,
+        'body'        => array(),
+        'httpversion' => '1.0',
+        'blocking'    => true,
+        'redirection' => 15,
+        )
+    );
+
+    if ( is_wp_error( $response ) ) {
+        $error_message = $response->get_error_message();
+        WC_Admin_Settings::add_error("Er ging iets mis: $error_message");
+    } else {
+        WC_Admin_Settings::add_error("AccessClient is met succes geactiveerd.");
+        $response_body = wp_remote_retrieve_body($response);
+        $json = json_decode($response_body);
+
+        return $json->token;
+    }
+}
+
+function includeHeaders($req) {
+    $headers = array('Content-Type: application/json');
+
+    if (!empty($config['accessClient'])) {
+        array_push(
+            $headers, 
+            "Access-Client-Token: {$ue_config['accessClient']}"
+        );
+    } else {
+        curl_setopt($req, CURLOPT_USERPWD, "{$ue_config['user']}: {$ue_config['password']}");
+    }
+
+    curl_setopt($req, CURLOPT_HTTPHEADER, $headers);
+}
+
+// function validateResponse($req, $res) {
+//     if ($res === false) {
+//         $info = curl_getinfo($req);
+//         curl_close($req);
+//         echo 'Er is een foutmelding opgetreden: ';
+//         echo '<pre>' . print_r($info) . '</pre>';
+//         die();
+//     }
+// }
+
+function debugConsole($output) {    
+    echo "<script>console.log(" . json_encode($output, JSON_HEX_TAG) . ");</script>";
 }
 
 ?>
